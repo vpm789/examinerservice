f9ca3a8a1cd58ac58a5888c0b2a839f5dd0f63c6
vpm789 <vpm789@gmail.com>
2023-06-30 14:54:14 +0300

    init

---
+++ src/main/java/pro/sky/java/course2/examinerservice/ExaminerserviceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examinerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminerserviceApplication.class, args);
+    }
+
+}
